1673579e367f74a420f7195de2c7589571cae855
ppsahu <pratyushpsahu2@gmail.com>
2025-05-03 21:30:10 -0700

    Added Java-Spring backend files.

---
+++ src/main/java/com/example/eCommerce_project/service/ProductService.java
@@ -0,0 +1,48 @@
+package com.example.eCommerce_project.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.eCommerce_project.model.Product;
+import com.example.eCommerce_project.repository.ProductRepo;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException{
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImagetype(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageDate(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImagetype(imageFile.getContentType());
+        return repo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return repo.searchProducts(keyword);
+    }
+}
